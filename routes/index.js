9c659e648b369d6b28193485af7628ff272e1b14
Alexis <alexis.moulin@efrei.net>
2019-12-16 19:04:50 +0100

    connection bdd + début authentification

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 const mtg = require('mtgsdk');
 const https = require('https');
+var sql = require('mssql')
+var mysql = require('mysql')
 var Request = require("request");
 var router = express.Router();
+
+
+const connect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cardswap"
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +22,25 @@ router.get('/', function(req, res, next) {
 router.get('/findcardView', function(req, res, next) {
   res.render('findcardView', {page:'Find a Card', 
                               menuId:'findCard',
-                              imageUrl: 'some url'});
+                              imageUrl: ''});
+});
+
+router.post('/api/login/', function(req, res) {
+  const username = req.body.username
+  const password = req.body.password
+
+  connect.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+    connect.query('SELECT * FROM user', function(err, result) {
+      if(err) console.log(err)
+      if(result[0].name === username && result[0].password === password){
+        res.render('findcardView', {page:'Find a Card', 
+                                    menuId:'findCard',
+                                    imageUrl: ''});
+      }
+    });
+  });
 });
 
 router.post('/api/findCard/', function(req, res) {
